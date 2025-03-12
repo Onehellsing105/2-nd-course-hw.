ecf57bf1712082f80a0c6d0e4333760ea0ff97aa
Onehellsing105 <olenniovivan@gmail.com>
2025-03-12 16:21:11 +0300

    подключение игры через js

---
+++ script.js
@@ -0,0 +1,6 @@
+document.addEventListener('DOMContentLoaded', function() {
+    var button = document.getElementById('opengame-1');
+    button.addEventListener('click', function() {
+        window.open('guessthenumber/index.html');
+    });
+});

e2a8dfaefd354a5d354066d7ac6639c463d48e13
Onehellsing105 <olenniovivan@gmail.com>
2025-03-13 21:10:07 +0300

    удалил лишнее, убрал ненужные отступы

--- script.js
+++ script.js
@@ -4,9 +4,16 @@ document.getElementById('opengame-1').addEventListener('click', function() {
 
 document.getElementById('opengame-1_mobile').addEventListener('click', function() {
     fetch('guessthenumber/index.html')
-        .then(response => response.text())
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Сеть ответила ошибкой: ' + response.statusText);
+            }
+            return response.text();
+        })
         .then(html => {
             document.body.innerHTML = html;
         })
-        .catch(error => console.error('Ошибка загрузки игры:', error));
+        .catch(error => {
+            console.error('Ошибка загрузки игры:', error);
+        });
 });
